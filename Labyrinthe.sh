084688c7a96c2168f9d96053f708e55e0682d113
Stephanie <stephcorpuz@gmail.com>
2020-11-15 16:17:20 +0100

    MAZE up to date

--- Labyrinthe.sh
+++ Labyrinthe.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
 echo "*********************************************"
 echo "************ Welcome to the MAZE ************"
 echo "*********************************************"
@@ -16,6 +15,19 @@ do
     if [ $room_number -eq 1 ]
     then
 	echo "There are 2 doors in your room: (n)orth, (e)ast, where do you want to go ?"
+	echo ""
+	echo ""
+	echo "............|  ^  |............"
+	echo "|                             |"
+	echo "|                             |"
+	echo "|                            ---"
+	echo "|           ╚(ಠ_ಠ)=┐          > "
+	echo "|                            ---"
+	echo "|                             |"
+	echo "|                             |"
+	echo "..............................."
+	echo ""
+	echo ""
 	read choice
 	if [ "$choice" = "n" ]
 	then
@@ -33,6 +45,19 @@ do
     elif [ $room_number -eq 2 ]
     then
 	echo "There are 3 doors in your room: (n)orth, (w)est, (e)ast, where do you want to go ?"
+	echo ""
+	echo ""
+	echo "............|  ^  |............"
+	echo "|                             |"
+	echo "|                             |"
+	echo "---                         ---"
+	echo " <         ᕕ( ಠ‿ಠ)ᕗ         > "
+	echo "---                         ---"
+	echo "|                             |"
+	echo "|                             |"
+	echo "..............................."
+	echo ""
+	echo ""
 	read choice
 	if [ "$choice" = "n" ]
 	then
@@ -56,6 +81,19 @@ do
     then
 	# TODO: complete
 	echo "There are 2 doors in your room: (n)orth, (w)est, where do you want to go ? "
+	echo ""
+	echo ""
+	echo "............|  ^  |............"
+	echo "|                             |"
+	echo "|                             |"
+	echo "---                           |"
+	echo " <         ᕙ(⇀‸↼‶)ᕗ          |"
+	echo "---                           |"
+	echo "|                             |"
+	echo "|                             |"
+	echo "..............................."
+	echo ""
+	echo ""
 	read choice
 	if [ "$choice" = "n" ]
 	then
@@ -73,22 +111,45 @@ do
     elif [ $room_number -eq 4 ]
     then
 	echo ">>>>>>>>>>>> It's a trap... you are DEAD :/"
+	echo ""
+	echo "........(╯︵╰,).........."
+	echo ""
 	game_running=falses
     elif [ $room_number -eq 5 ]
     then
-# TODO: complete
 	echo "There is 1 door in your room: (s)outh, where do you want to go?"
+	echo ""
+	echo ""
+	echo "..............................."
+	echo "|                             |"
+	echo "|                             |"
+	echo "|                             |"
+	echo "|            ಠ╭╮ಠ             |"
+	echo "|                             |"
+	echo "|                             |"
+	echo "|                             |"
+	echo "............|  v  |............"
+	echo ""
+	echo ""
 	read choice
 	if [ "$choice" = "s" ]
 	then
 	    clear
 	    echo "> going south..."
+		room_number=2
 	else
 	    echo "THIS CHOICE DOESN'T EXIST"
 	fi
     elif [ $room_number -eq 6 ]
     then
+	echo ""
+	echo ""
 	echo ">>>>>>>>>>>> You found the EXIT... Congratulations :)"
+	echo ""
+	echo ""
+	echo ".................╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ.................."
+	echo ""
+	echo ""
 	read choice
 	game_running=falses
     fi
